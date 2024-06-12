556797876d5e936fd538984a0d7eae4998584b70
Naomy95 <zannatnaomy@gmail.com>
2024-06-12 19:11:06 +0200

    doctor page ready

--- src/lib/api/doctor.js
+++ src/lib/api/doctor.js
@@ -2,14 +2,26 @@ import axios from "axios"
 
 
 export const getDoctors = async () => {
-    const res = await axios.get('http://localhost:5001/doctors/', {
+    const res = await axios.get('http://localhost:4000/doctors/', {
       headers: { 'Content-Type': 'application/json' },
     })
     return res.data
   }
+  export const postDoctor = (data) => {
+    return  axios.post(`http://localhost:4000/createDoctor`,data, {
+      headers: { 'Content-Type': 'application/json' },
+    })
 
-  export const updateDoctor = ({id,doctor:doctor}) => {
-    return  axios.put(`http://localhost:5001/doctors/:${id}`,{doctor:doctor}, {
+  }
+
+  export const updateDoctor = (id,doctor) => {
+    return  axios.put(`http://localhost:4000/doctor/${id}`,doctor, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+  }
+  export const deleteDoctor = (id) => {
+    return  axios.delete(`http://localhost:4000/doctor/${id}`, {
       headers: { 'Content-Type': 'application/json' },
     })
 
--- src/lib/api/nurse.js
+++ src/lib/api/nurse.js
@@ -2,14 +2,26 @@ import axios from "axios"
 
 
 export const getNurses = async () => {
-    const res = await axios.get('http://localhost:5001/nurses/', {
+    const res = await axios.get('http://localhost:4000/nurses/', {
       headers: { 'Content-Type': 'application/json' },
     })
     return res.data
   }
+  export const postNurse = (data) => {
+    return  axios.post(`http://localhost:4000/createNurse`,data, {
+      headers: { 'Content-Type': 'application/json' },
+    })
 
-  export const updateDoctor = ({id,doctor:doctor}) => {
-    return  axios.put(`http://localhost:5001/doctors/:${id}`,{doctor:doctor}, {
+  }
+
+  export const updateNurse = (id,nurse) => {
+    return  axios.put(`http://localhost:4000/nurse/${id}`,nurse, {
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+  }
+  export const deleteNurse = (id) => {
+    return  axios.delete(`http://localhost:4000/nurse/${id}`, {
       headers: { 'Content-Type': 'application/json' },
     })
 
